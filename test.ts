8cd214620819a514d8ff538b7e9da2f16079b345
Jose M Martinez <josemanuelmartinezduran@gmail.com>
2023-12-22 10:18:14 -0600

    c

--- test.ts
+++ test.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.less']
 })
 export class TestComponent {
   a:string = "Hola";
