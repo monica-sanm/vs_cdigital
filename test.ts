094588962b40d0eb5abde8f8a13a0db5a2ee6184
Jose M Martinez <josemanuelmartinezduran@gmail.com>
2023-12-22 10:34:13 -0600

    a

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class TestComponent {
   a:string = "Hola";
+  @Input() aplicacion:string = "";
 }
